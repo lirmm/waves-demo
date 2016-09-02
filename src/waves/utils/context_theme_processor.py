d14845145ba2129a5dc3bc0a107bb15a12b47ba5
Marc Chakiachvili <marc.chakiachvili@lirmm.fr>
2016-09-02 09:58:35 +0200

    Reactivate context theme processor class (to add in classic Django settings file if needed)

---
+++ src/waves/utils/context_theme_processor.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.contrib.sites.shortcuts import get_current_site as current_site
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from waves.models.site import WavesSite
+import waves.settings
+
+
+def css_theme(request):
+    try:
+        site = current_site(request)
+        wave_site = WavesSite.on_site.get(site=site)
+        if wave_site:
+            current_theme = wave_site.theme
+    except (ObjectDoesNotExist, MultipleObjectsReturned, AttributeError):
+        # get default from settings
+        current_theme = waves.settings.WAVES_BOOTSTRAP_THEME
+    return {'BOOTSTRAP_CSS_THEME': current_theme}
