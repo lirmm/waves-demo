13eb4825b77803d45864c67287572b731c6b280b
Marc Chakiachvili <marc.chakiachvili@lirmm.fr>
2016-12-01 15:34:16 +0100

    Moved setup.py

---
+++ setup.py
@@ -0,0 +1,55 @@
+from distutils.core import setup
+
+import os
+from setuptools import setup, find_packages
+mod_root = "src/waves"
+
+
+def get_version(mod_root):
+    try:
+        import waves
+        _version = getattr(waves, '__version__', None)
+        _version_detail = getattr(waves, '__version_details__', None)
+        _dist_name = getattr(waves, '__name__', 'WAVES')
+    except ImportError:
+        _version = None
+        _version_detail = None
+        _dist_name = None
+    return _version, _version_detail, _dist_name
+
+version, version_detail, sdist_name = get_version(mod_root)
+
+setup(
+    name='waves-webapp',
+    version=version,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/lirmm/waves-webapp',
+    license='GPL-v3',
+    author='Marc Chakiachvili, Floreal Cabanattes, Vincent Berry, Anne-Muriel Arigon Chifolleau',
+    author_email='marc.chakiachvili@gmail.com, '
+                 'floreal.cabanettes@lirmm.fr, '
+                 'vincent.lefort@lirmm.fr, '
+                 'vincent.berryr@lirmm.fr, '
+                 'anne-muriel.arigon@lirmm.fr',
+    description='WebApp for Versatile and Easy bio-informatics Services',
+    maintainer='LIRMM - MAB Laboratory - France',
+    maintainer_email='vincent.lefort@lirmm.fr',
+    classifiers=[
+        'Development Status :: 1 - Dev/Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Topic :: Web :: Distributed Computing',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix'
+    ],
+
+)
