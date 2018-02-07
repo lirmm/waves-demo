17ac5aa9d5231bc72e1d4d8cba8679098310235b
Marc Chjakiachvili <marc.chakiachvili@lirmm.fr>
2018-02-07 13:32:27 +0100

    Updated templates

--- src/demo/templatetags/demo_tags.py
+++ src/demo/templatetags/demo_tags.py
@@ -1,10 +1,16 @@
 from __future__ import unicode_literals
 
 from django import template
+from demo import __version_detail__
 
 register = template.Library()
 
 
 @register.simple_tag
-def get_app_name():
+def get_demo_name():
     return "WAVES DEMO APPLICATION"
+
+@register.simple_tag
+def get_demo_version():
+    return __version_detail__
+
--- src/waves_demo/settings/base.py
+++ src/waves_demo/settings/base.py
@@ -167,10 +167,4 @@ WCORE_SUBMISSION_MODEL = 'demo.DemoWavesSubmission'
 
 
 # MAILS
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = 'WAVES <waves-demo@atgc-montpellier.fr>'
