81f581c1dd26ce4f8794fc190d985dec89fb2157
Marc Chjakiachvili <marc.chakiachvili@lirmm.fr>
2018-02-14 16:06:29 +0100

    Last css touch !

--- src/demo/templatetags/demo_tags.py
+++ src/demo/templatetags/demo_tags.py
@@ -14,3 +14,7 @@ def get_demo_name():
 def get_demo_version():
     return __version_detail__
 
+
+@register.inclusion_tag('demo/run_details.html', takes_context=False)
+def job_run_details(job=None):
+    return {'run_details': vars(job.run_details).items()}
